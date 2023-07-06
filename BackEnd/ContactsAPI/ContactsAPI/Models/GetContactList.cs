1fe8dd084f15d46ac92556de5722c21527846713
Joel Manuel <devjoelmanuel@gmail.com>
2023-07-06 22:41:15 +0800

    separate models for getting a list of contacts

---
+++ BackEnd/ContactsAPI/ContactsAPI/Models/GetContactList.cs
@@ -0,0 +1,10 @@
+﻿namespace ContactsAPI.Models
+{
+    public class GetContactList
+    {
+        public int Page { get; init; } = 1;
+        public int PageSize { get; init; } = 10;
+        public string Query { get; init; } = string.Empty;
+        public Sort SortBy { get; init; } = Sort.ASC;
+    }
+}
--- BackEnd/ContactsAPI/ContactsAPI/Models/PaginatedList.cs
+++ BackEnd/ContactsAPI/ContactsAPI/Models/PaginatedList.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 
 namespace ContactsAPI.Models
 {
+    public enum Sort
+    { 
+        ASC, DESC
+    }
+
     public class PaginatedList<T>
     {
         public IEnumerable<T> Data { get; init; }
         public int Page { get; init; }
         public int PageSize { get; init; }
+        public int TotalRecordCount { get; init; }
     }
 }
