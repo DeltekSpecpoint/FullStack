361afd5de4d77333035c1043f8e6f5ddf5c4da15
Carlos Catalan <ccatalan6@gatech.edu>
2023-07-10 02:17:11 +0800

    skeleton edit code

--- contacts-app/src/Contacts/Contact.js
+++ contacts-app/src/Contacts/Contact.js
@@ -10,31 +10,87 @@ function Contact(props) {
   
 
   const {contact, onHandleDelete} = props;
-  console.log('contact', contact)
+  const [editMode, setEditMode] = useState(false);
+  const [editName, setEditName] = useState(contact.name);
+  const [editContactNo, setEditContactNo] = useState(contact.phone);
+  const [editEmail, setEditEmail] = useState(contact.email);
 
   const handleDelete = () => {
     onHandleDelete(contact);
   }
+
+  const handleSetEdit = () => {
+    setEditMode(true);
+  }
+
+  const handleCancelSetEdit = () => {
+    setEditMode(false);
+  }
+
+  const handleSaveChanges = () => {
+
+  }
  
+  
 
   return (
     <div className="contact">
       <div className="contactName">
           <img src={userIcon} width="100" height="100" alt="user" />
-          <span>{contact.name}</span>
+          {
+            editMode ? 
+            <div className="editField">
+              <label for="editName">Name:</label>
+                <input 
+                type="text" 
+                id="editName" 
+                name="editName"
+                value={editName}
+                onChange={(e) => setEditName(e.target.value)}
+              />
+            </div> : 
+            <span>{contact.name}</span>
+          }
       </div>
       <div className="contactDetails">
           <div className="contactInfo">
               <img src={phone} width="40" height="40" alt="phone" />
-              <span>{contact.phone}</span>
+              {
+                editMode ? 
+                <div className="editField">
+                  <label for="editContactNo">Contact No:</label>
+                    <input 
+                    type="text" 
+                    id="editContactNo" 
+                    name="editContactNo"
+                    value={editContactNo}
+                    onChange={(e) => setEditContactNo(e.target.value)}
+                  />
+                </div> : 
+                <span>{contact.phone}</span>
+              }
           </div>
           <div className="contactInfo">
               <img src={mail} width="40" height="40" alt="email" />
-              <span>{contact.email}</span>
+              {
+                editMode ? 
+                <div className="editField">
+                  <label for="editEmail">Email:</label>
+                    <input 
+                    type="text" 
+                    id="editEmail" 
+                    name="editEmail"
+                    value={editEmail}
+                    onChange={(e) => setEditEmail(e.target.value)}
+                  />
+                </div> : 
+                <span>{contact.email}</span>
+              }
           </div>      
       </div>
       <div className="actions">
-          <button>Edit</button> 
+          {editMode ? <button onClick={handleCancelSetEdit}>Cancel</button> : <button onClick={handleSetEdit}>Edit</button>} 
+          {editMode && <button onClick={handleSaveChanges}>Save</button>}
           <button onClick={handleDelete}>Delete</button>  
       </div>
     </div>
