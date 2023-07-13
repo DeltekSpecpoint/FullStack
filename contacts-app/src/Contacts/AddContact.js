dad17d37dbc75b2ac3c2e05848b77a707c356d81
Carlos Catalan <ccatalan6@gatech.edu>
2023-07-14 01:37:19 +0800

    form validation for add contacts

--- contacts-app/src/Contacts/AddContact.js
+++ contacts-app/src/Contacts/AddContact.js
@@ -8,13 +8,17 @@ function AddContact(props) {
   const [name, setName] = useState("");
   const [contactNo, setContactNo] = useState("");
   const [email, setEmail] = useState("");
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+
+  const [isEmailError, setIsEmailError] =  useState(false);
+  const [isContactNoError, setIsContactNoError] = useState(false);
+
+  useEffect(() => {
+    setButtonDisabled(isContactNoError||isEmailError);
+  }, [isEmailError, isContactNoError]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('submit')
-    console.log('name', name)
-    console.log('contactNo', contactNo)
-    console.log('email', email)
 
     handleAddContacts({
       name: name,
@@ -22,6 +26,35 @@ function AddContact(props) {
       email: email
     })
   }
+
+
+
+  const handleSetEmail = (emailValue) => {
+
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+    if(!emailRegex.test(emailValue)){
+      setIsEmailError(true)
+    } else {
+      setIsEmailError(false);
+    }
+
+    setEmail(emailValue);
+  }
+
+  const handleSetPhone = (phoneValue) => {
+    const phoneRegex = /[0-9]{3}-[0-9]{2}-[0-9]{3}/;
+
+    if(!phoneRegex.test(phoneValue)){
+      setIsContactNoError(true)
+    } else {
+      setIsContactNoError(false);
+    }
+
+    setContactNo(phoneValue);
+  }
+
+
 
   return (
     <form className="addContactContainer">
@@ -41,27 +74,31 @@ function AddContact(props) {
         <div>
           <label for="contactNo">Contact No:</label>
           <input 
-            type="text" 
+            type="tel" 
             id="contactNo" 
             name="contactNo"
-            aria-label="Phone Number"
+            aria-label="Contact Number"
             value={contactNo}
-            onChange={(e) => setContactNo(e.target.value)}
+            pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+            onChange={(e) => handleSetPhone(e.target.value)}
           />
+          {isContactNoError && (<span className="errorMessage">Must follow format: xxx-xx-xxx</span>)}
         </div>
         <div>
           <label for="email">Email:</label>
           <input 
-            type="text" 
+            type="email" 
             id="email" 
             name="email"
             aria-label="Email Address"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => handleSetEmail(e.target.value)}
+            className={isEmailError ? 'errorField': 'emailField'}
           />
+          {isEmailError && (<span className="errorMessage">Email address format invalid</span>)}
         </div>
         <div>
-          <button aria-label="Add Contact" className="submitButton" onClick={handleSubmit}>Submit</button>
+          <button type="button" aria-label="Add Contact" disabled={buttonDisabled} className="submitButton" onClick={handleSubmit}>Submit</button>
         </div> 
       </form>
     </form>
--- contacts-app/src/Contacts/ContactList.js
+++ contacts-app/src/Contacts/ContactList.js
@@ -10,25 +10,7 @@ function ContactList(props) {
 
   return (
     <div className="contactList">
-      <h1>Contact List</h1>
-      <form className="contactListControls">
-        <div>
-          <label for="Search">Search:</label>
-          <input 
-                type="text" 
-                id="name" 
-                name="search"
-                aria-label="Search Input"
-          />
-        </div>
-        <div>
-          <label for="sort">Sort by:</label>
-          <select id="sort" name="sort">
-            <option value="ascending">Ascending</option>
-            <option value="descending">Descending</option>
-          </select>
-        </div>
-      </form>  
+      <h1>Contact List</h1>  
         {
           contactList.map(contact=> {
             return (
