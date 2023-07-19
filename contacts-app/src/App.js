3bceec20abac7a92036d551a057f422abcbd6214
yournewdev <anjbicaldo@gmail.com>
2023-07-19 11:39:15 +0800

    chore: add navigation bar

--- contacts-app/src/App.js
+++ contacts-app/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import NavBar from "./components/nav-bar/NavBar";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <NavBar />
+    </BrowserRouter>
   );
 }
 
