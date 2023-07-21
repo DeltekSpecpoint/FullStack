c262e0a95318cd280879b1810421d67c78130896
yournewdev <anjbicaldo@gmail.com>
2023-07-21 20:05:14 +0800

    chore: refactor code to dynamically validate the form for both edit and create page

--- contacts-app/src/components/edit-contact/EditContact.jsx
+++ contacts-app/src/components/edit-contact/EditContact.jsx
@@ -5,8 +5,6 @@ import { useNavigate, useParams } from 'react-router-dom';
 
 const EditContact = () => {
   const [contact, setContact] = useState({});
-  const [formErrors, setFormErrors] = useState({});
-  const [isSubmit, setIsSubmit] = useState(false)
   const { id } = useParams();
 
   let navigate = useNavigate();
@@ -25,45 +23,11 @@ const EditContact = () => {
     e.preventDefault();
     setContact({...contact, [e.target.name]: e.target.value});
   };
-  
-  const editContactDetails = () => {
-    setFormErrors(validate(contact));
-    setIsSubmit(true);
-  }
-  
-  const finishSubmit = () => {
+
+  const handleSubmit = () => {
     editContact(id, contact);
     navigate('/');
   };
-  useEffect(() => {
-    if (Object.keys(formErrors).length === 0 && isSubmit) {
-      finishSubmit();
-    }
-  }, [formErrors]);
-  
-  const validate = (contact) => {
-    let errors = {};
-    const nameRegex = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
-    const numberRegex = /((\+|00)?[1-9]{2}|0)[1-9]([0-9]){8}/;
-
-    if (!contact.name){
-      errors.name = "Name is required!";
-    }
-    if (!contact.number){
-      errors.number = "Number is required!";
-    } else if(!numberRegex.test(contact.number)){
-      errors.number = "This is not a valid phone number!";
-    }
-    if (!contact.email){
-      errors.email = "Email is required!";
-    } else if(!nameRegex.test(contact.email)){
-      errors.email = "This is not a valid email format!";
-    }
-    if (!contact.address){
-      errors.address = "Address is required!";
-    }
-    return errors;
-  }
 
   return (
     <Form
@@ -73,16 +37,8 @@ const EditContact = () => {
       number={contact.number}
       email={contact.email}
       address={contact.address}
-      onClick={() => editContactDetails()}
-      nameError={Boolean(formErrors.name)}
-      nameErrorText={formErrors.name}
-      numberError={Boolean(formErrors.number)}
-      numberErrorText={formErrors.number}
-      emailError={Boolean(formErrors.email)}
-      emailErrorText={formErrors.email}
-      addressError={Boolean(formErrors.address)}
-      addressErrorText={formErrors.address}
       hiddenLabel={true}
+      finishSubmit={() => handleSubmit()}
     />
   )
 }
--- contacts-app/src/components/form/Form.jsx
+++ contacts-app/src/components/form/Form.jsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Form.css";
 import { Button, FormControl, FormGroup, TextField, Typography } from '@mui/material';
 
 const Form = (props) => {
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false)
+
+  const validate = () => {
+    let errors = {};
+    const nameRegex = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
+    const numberRegex = /((\+|00)?[1-9]{2}|0)[1-9]([0-9]){8}/;
+    
+    if (!props.name){
+      errors.name = "Name is required!";
+    }
+    if (!props.number){
+      errors.number = "Number is required!";
+    } else if(!numberRegex.test(props.number)){
+      errors.number = "This is not a valid phone number!";
+    }
+    if (!props.email){
+      errors.email = "Email is required!";
+    } else if(!nameRegex.test(props.email)){
+      errors.email = "This is not a valid email format!";
+    }
+    if (!props.address){
+      errors.address = "Address is required!";
+    }
+    setFormErrors(errors);
+    setIsSubmit(true);
+    return errors;
+  }
+  
+  useEffect(() => {
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      props.finishSubmit();
+    }
+  }, [formErrors, isSubmit]);
 
   return (
     <FormGroup className='form-grp'>
@@ -17,8 +51,8 @@ const Form = (props) => {
         }
         id='name-input'
         required={true}
-        error={props.nameError}
-        helperText={props.nameErrorText}
+        error={Boolean(formErrors.name)}
+        helperText={formErrors.name}
         />
       </FormControl>
       <FormControl className='form-ctrl'>
@@ -31,8 +65,8 @@ const Form = (props) => {
         }
         id='number-input'
         required={true}
-        error={props.numberError}
-        helperText={props.numberErrorText}
+        error={Boolean(formErrors.number)}
+        helperText={formErrors.number}
         />
       </FormControl>
       <FormControl className='form-ctrl'>
@@ -45,8 +79,8 @@ const Form = (props) => {
         }
         id='email-input'
         required={true}
-        error={props.emailError}
-        helperText={props.emailErrorText}
+        error={Boolean(formErrors.email)}
+        helperText={formErrors.email}
         />
       </FormControl>
       <FormControl className='form-ctrl'>
@@ -59,12 +93,12 @@ const Form = (props) => {
         }
         id='address-input'
         required={true}
-        error={props.addressError}
-        helperText={props.addressErrorText}
+        error={Boolean(formErrors.address)}
+        helperText={formErrors.address}
         />
       </FormControl>
       <FormControl className='form-ctrl'>
-        <Button variant='contained' color='primary' onClick={props.onClick} >
+        <Button variant='contained' color='primary' onClick={ () => validate()} >
           Submit
         </Button>
       </FormControl>
