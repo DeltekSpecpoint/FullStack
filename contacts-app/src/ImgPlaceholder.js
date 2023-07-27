f0f5ed7a73b78c9fb5ef3f40d8951da1911c9c84
jcada05 <76962892+jcada05@users.noreply.github.com>
2023-07-28 00:48:29 +0800

    Initial commit for Frontend

---
+++ contacts-app/src/ImgPlaceholder.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Placeholder from './placeholder.png'
+
+function ImgPlaceholder() {
+  return (
+    <div><img src={Placeholder}></img></div>
+  )
+}
+
+export default ImgPlaceholder
